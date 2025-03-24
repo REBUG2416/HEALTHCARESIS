ac8c0b0cafd494a1e1413b5c96c380d7c0876379
REBUG2416 <davidagochukwu@gmail.com>
2025-03-24 20:54:14 +0100

    PUSHHHH

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const app = express();
